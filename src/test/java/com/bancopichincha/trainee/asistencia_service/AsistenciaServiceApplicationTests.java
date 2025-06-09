a0f16c4e41f0ac8c436d8ff46cf45c6d287f25cd
Jeffers199817 <jefferson.alquinga1998@gmail.com>
2025-06-08 22:52:45 -0500

    Integración del proyecto registro de estudiantes

---
+++ src/test/java/com/bancopichincha/trainee/asistencia_service/AsistenciaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancopichincha.trainee.asistencia_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
